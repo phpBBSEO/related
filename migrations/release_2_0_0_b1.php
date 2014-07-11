a1d19595ff3d6884bf5eba29e5e223a300303f2e
Carlo <carlo@phpbbitalia.net>
2014-07-11 16:25:52 +0200

    Code style fix

--- migrations/release_2_0_0_b1.php
+++ migrations/release_2_0_0_b1.php
@@ -13,6 +13,7 @@ namespace phpbbseo\related\migrations;
 class release_2_0_0_b1 extends \phpbb\db\migration\migration
 {
 	const SQL_FULLTEXT_NAME = 'topic_tft';
+
 	public function effectively_installed()
 	{
 		return !empty($this->config['seo_related_on']);
@@ -26,30 +27,36 @@ class release_2_0_0_b1 extends \phpbb\db\migration\migration
 	public function update_schema()
 	{
 		global $db;
+
 		$db_tools = new \phpbb\db\tools($db);
 		$indexes = $db_tools->sql_list_index(TOPICS_TABLE);
 		$fulltext = 0;
-		if (!in_array(self::SQL_FULLTEXT_NAME, $indexes)) {
+
+		if (!in_array(self::SQL_FULLTEXT_NAME, $indexes))
+		{
 			// do not use db_tools since it does not support to add FullText indexes
 			// we also use quite a basic approach to make sure that the index is not refused for bad reasons
 			$sql = 'ALTER TABLE ' . TOPICS_TABLE . '
 				ADD FULLTEXT ' . self::SQL_FULLTEXT_NAME . ' (topic_title)';
 			$db->sql_return_on_error(true);
 			$db->sql_query($sql);
+
 			// make *sure* about the index !
 			$indexes = $db_tools->sql_list_index(TOPICS_TABLE);
 			$fulltext = in_array(self::SQL_FULLTEXT_NAME, $indexes) ? 1 : 0;
 			$db->sql_return_on_error(false);
 		}
+
 		set_config('seo_related_fulltext', $fulltext);
+
 		return array();
 	}
 
 	public function revert_schema()
 	{
 		return array(
-			'drop_keys' => array(
-				TOPICS_TABLE => array(
+			'drop_keys'	=> array(
+				TOPICS_TABLE	=> array(
 					self::SQL_FULLTEXT_NAME,
 				),
 			),
